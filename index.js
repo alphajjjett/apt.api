1416ce291537aba5a481c9432676d2cc136c5ece
Suttipat Joysrithong <jetbrabore@hotmail.com>
2024-11-16 18:38:24 +0700

    commit

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const Product = require('./models/product.model.js')
+app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-
-});
 
 app.get('/', (req, res) => {
-    res.send("Hello form node api server");
+    res.send("Hello form node api");
 });
+
+
+app.post('/api/products',async (req, res) => {
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    }catch (error){
+        res.status(500).json({message :error.message})
+    }
+});
+
+
+mongoose.connect("mongodb+srv://suttipat2453:jet10220@nodeapi.njaen.mongodb.net/Node-API?retryWrites=true&w=majority&appName=NodeAPI")
+.then(() => {
+    console.log("connect to database !");
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    
+    });
+})
+
+.catch(() =>{
+    console.log("Connection Failed!");
+})

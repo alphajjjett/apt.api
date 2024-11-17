2c24297bd02e3d80102ee957f7a0f90650c8cf9c
Suttipat Joysrithong <jetbrabore@hotmail.com>
2024-11-17 10:14:12 +0700

    commit

--- index.js
+++ index.js
@@ -10,9 +10,20 @@ app.get('/', (req, res) => {
 });
 
 
+app.get('/api/products',async (req, res) =>{
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch(error){
+        res.status(500).json({
+            message:error.message
+        });
+    }
+});
+
 app.post('/api/products',async (req, res) => {
     try{
-        const product = await Product.create(req.body);
+        const products = await Product.create(req.body);
         res.status(200).json(product);
     }catch (error){
         res.status(500).json({message :error.message})

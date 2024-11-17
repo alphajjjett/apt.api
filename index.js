9a9aecb0c01250c67f4d83cac77df6808e81f38f
Suttipat Joysrithong <jetbrabore@hotmail.com>
2024-11-17 21:17:43 +0700

    commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.send("Hello form node api");
 });
 
-
+//ค้นข้อมูล
 app.get('/api/products',async (req, res) =>{
     try{
         const products = await Product.find({});
@@ -21,9 +21,21 @@ app.get('/api/products',async (req, res) =>{
     }
 });
 
+
+//ดึงข้อมูลแค่บาง id
+app.get('/api/product/:id',async (req, res) => {
+    try{
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    }catch (error){
+        res.status(500).json({message :error.message});
+    }
+});
+
 app.post('/api/products',async (req, res) => {
     try{
-        const products = await Product.create(req.body);
+        const product = await Product.create(req.body);
         res.status(200).json(product);
     }catch (error){
         res.status(500).json({message :error.message})
@@ -31,12 +43,58 @@ app.post('/api/products',async (req, res) => {
 });
 
 
+
+//อัพเดทข้อมูล
+app.put('/api/product/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!product){
+            return res.status(404).json({message : "Product not found!"});
+        }
+
+        const updateProduct = await Product.findById(id);
+        res.status(200).json(updateProduct);
+
+    } catch(error){
+        res.status(500).json({message : error.message});
+    }
+
+});
+
+
+//ลบข้อมูล
+
+app.delete('/api/product/:id', async (req, res)=>{
+    try{
+        const {id} = req.params;
+
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({message : "Product not found!"})
+        }
+
+        res.status(200).json({message : "Product deleted successfully"})
+
+    }catch(error){
+
+        res.status(500).json({message : error.message});
+    }
+});
+
+
+
+
+
+
 mongoose.connect("mongodb+srv://suttipat2453:jet10220@nodeapi.njaen.mongodb.net/Node-API?retryWrites=true&w=majority&appName=NodeAPI")
 .then(() => {
     console.log("connect to database !");
     app.listen(3000, () => {
         console.log('Server is running on port 3000');
-    
     });
 })
 

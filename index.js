39ddef34d86e88d0a07d060e16d808fe33cb2217
Suttipat Joysrithong <jetbrabore@hotmail.com>
2024-11-18 13:10:28 +0700

    commit

--- index.js
+++ index.js
@@ -1,73 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const Product = require('./models/product.model.js')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const Product = require('./models/product.model.js');
+const productRoute = require('./routes/product.route.js');
+
 app.use(express.json());
+
+app.use(express.urlencoded({extended: false}));
+
+
+
+// middleware
+app.use("/api/products", productRoute);
+
 
 
 app.get('/', (req, res) => {
     res.send("Hello form node api");
 });
 
-//ค้นข้อมูล
-app.get('/api/products',async (req, res) =>{
-    try{
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch(error){
-        res.status(500).json({
-            message:error.message
-        });
-    }
-});
-
-
-//ดึงข้อมูลแค่บาง id
-app.get('/api/product/:id',async (req, res) => {
-    try{
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    }catch (error){
-        res.status(500).json({message :error.message});
-    }
-});
-
-app.post('/api/products',async (req, res) => {
-    try{
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    }catch (error){
-        res.status(500).json({message :error.message})
-    }
-});
-
-
-
-//อัพเดทข้อมูล
-app.put('/api/product/:id', async (req, res) => {
-    try{
-        const {id} = req.params;
-
-        const product = await Product.findByIdAndUpdate(id, req.body);
-
-        if (!product){
-            return res.status(404).json({message : "Product not found!"});
-        }
-
-        const updateProduct = await Product.findById(id);
-        res.status(200).json(updateProduct);
-
-    } catch(error){
-        res.status(500).json({message : error.message});
-    }
-
-});
 
 
 //ลบข้อมูล
 
-app.delete('/api/product/:id', async (req, res)=>{
+app.delete('/api/products/:id', async (req, res)=>{
     try{
         const {id} = req.params;
 

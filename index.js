e5fb8f2daec5544f8f21479f240010d203aedd17
Suttipat Joysrithong <jetbrabore@hotmail.com>
2024-11-18 14:11:46 +0700

    commit

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const Product = require('./models/product.model.js');
 const productRoute = require('./routes/product.route.js');
 
+require('dotenv').config();
+const uri = process.env.MONGO_URI;
+console.log(uri);
+
 app.use(express.json());
 
 app.use(express.urlencoded({extended: false}));
@@ -19,41 +23,11 @@ app.get('/', (req, res) => {
     res.send("Hello form node api");
 });
 
-
-
-//ลบข้อมูล
-
-app.delete('/api/products/:id', async (req, res)=>{
-    try{
-        const {id} = req.params;
-
-        const product = await Product.findByIdAndDelete(id);
-
-        if (!product) {
-            return res.status(404).json({message : "Product not found!"})
-        }
-
-        res.status(200).json({message : "Product deleted successfully"})
-
-    }catch(error){
-
-        res.status(500).json({message : error.message});
-    }
-});
-
-
-
-
-
-
-mongoose.connect("mongodb+srv://suttipat2453:jet10220@nodeapi.njaen.mongodb.net/Node-API?retryWrites=true&w=majority&appName=NodeAPI")
-.then(() => {
-    console.log("connect to database !");
+mongoose.connect(uri)
+  .then(() => {
+    console.log("Connected to the database!");
     app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+      console.log('Server is running on port 3000');
     });
-})
-
-.catch(() =>{
-    console.log("Connection Failed!");
-})
+  })
+  .catch(err => console.error('Failed to connect to MongoDB', err));

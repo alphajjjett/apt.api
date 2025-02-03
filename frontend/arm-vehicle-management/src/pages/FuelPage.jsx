ff155dc2360ab4d17a4f99dbe89e37ca45f3cee2
Suttipat Joysrithong <jetbrabore@hotmail.com>
2025-02-03 23:36:17 +0700

    commit forntend

--- frontend/arm-vehicle-management/src/pages/FuelPage.jsx
+++ frontend/arm-vehicle-management/src/pages/FuelPage.jsx
@@ -331,19 +331,8 @@ const FuelPage = () => {
               onChange={handleSearch}
             />
           </div>
-        </div>  
+        </div>
         <TableContainer component={Paper}>
-          <TableRow>
-              {filteredFuelRecords.map((record) => (
-                <TableCell
-                  key={record.id}
-                  align={record.align}
-                  style={{ top: 57, minWidth: record.minWidth }}
-                >
-                  {record.label}
-                </TableCell>
-              ))}
-            </TableRow>
           <Table>
             <TableHead>
               <TableRow>
@@ -375,6 +364,7 @@ const FuelPage = () => {
                     atob(localStorage.getItem("token")?.split(".")[1])
                   ).selfid;
                   const isCurrentUser = user?.selfid === loggedInUserSelfid;
+
                   return (
                     <TableRow key={record._id}>
                       <TableCell align="left">{filteredFuelRecords.length - (page * rowsPerPage + index)}</TableCell>
